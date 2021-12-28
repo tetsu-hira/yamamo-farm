cf0a664e8194d7224d28d844482aae1b21426c13
tetsu.hira <svc.snowdrop@gmail.com>
2021-12-28 12:34:15 +0900

    全てgitにあげる

--- pages/Main.tsx
+++ pages/Main.tsx
@@ -12,54 +12,54 @@ export default function Main() {
   });
   
   return (
-    <>
-      <div className="mt-32 mx-auto">
-        <h2 className='text-3xl font-bold text-center'>銀賞獲得に向けての活動</h2>
-        <p className="text-2xl font-bold text-center leading-10 mt-5">
-          獲得に向けて今年度は
-          <br />現在の整頓状況を維持
+    <div className={styles.Main}>
+      <div className={styles.foreword}>
+        <h2>今年度目標：銀賞の獲得</h2>
+        <p className={styles.big}>
+          獲得に向けてまずは現在の
+          <br />整頓状況を維持していくことから
         </p>
-        <p className='text-lg font-thin mt-5 text-center'>
-          今年は銀賞を獲得するため現在の整頓状況を維持しながら
-          <br />より上のレベルを目指した活動を行う
+        <p className={styles.small}>
+          その上でさらに細かな箇所を改善していくことで
+          <br />銀賞の条件を達成して申請を行いたいと思います。
         </p>
       </div>
-      <div className='w-4/5 m-auto'>
-        <div className='mt-28 flex'>
-          <div className='w-1/2' ref={ref}>
+      <div className={styles.intro}>
+        <div className={styles.group}>
+          <div className={styles.image} ref={ref}>
             {inView && (
               <Image className='animate__animated animate__fadeInDown' src="/main1.jpg" width={540} height={360} alt="メイン画像1" />
 
             )}
           </div>
-          <div className='pl-20 pt-20 w-1/2'>
-            <h2 className='text-3xl my-1 mb-5'>日々の5S活動</h2>
-            <p className='text-lg font-thin'>生産管理室のホワイトボードに計画予定を貼って活動内容の意識を図ったり、前月の実績やクロスパトロールの結果を貼って進捗を見える化しています。</p>
+          <div className={styles.contentLeft}>
+            <h2>日々の5S活動</h2>
+            <p>生産管理室のホワイトボードに計画予定を貼って活動内容の意識を図ったり、前月の実績やクロスパトロールの結果を貼って進捗を見える化しています。</p>
           </div>
         </div>
-        <div className='mt-28 flex'>
-          <div className='pr-20 pt-20 w-1/2'>
-            <h2 className='text-3xl my-1 mb-5'>年に一度のPC清掃</h2>
-            <p className='text-lg font-thin'>昨年より年に一度の大掃除と別に日を設け、生産管理室で使っているデスクトップPCの清掃を行っております。</p>
+        <div className={styles.group}>
+          <div className={styles.contentRight}>
+            <h2>年に一度のPC清掃</h2>
+            <p>昨年より年に一度の大掃除と別に日を設け、生産管理室で使っているデスクトップPCの清掃を行っております。</p>
           </div>
-          <div className='w-1/2' ref={ref}>
+          <div className={styles.image} ref={ref}>
             {inView && (
               <Image className='animate__animated animate__fadeInDown' src="/main2.jpg" width={540} height={360} alt="メイン画像1" />
             )}
           </div>
         </div>
-        <div className='mt-28 flex'>
-          <div className='w-1/2' ref={ref}>
+        <div className={styles.group}>
+          <div className={styles.image} ref={ref}>
             {inView && (
               <Image className='animate__animated animate__fadeInDown' src="/main3.jpg" width={540} height={360} alt="メイン画像1" />
             )}
           </div>
-          <div className='pl-20 pt-20 w-1/2'>
-            <h2 className='text-3xl my-1 mb-5'>安全にも配慮</h2>
-            <p className='text-lg font-thin'>日々の整理整頓だけでなく、地震時にPCが倒れてこないようにするなどの安全対策も5S活動の一環として行います。</p>
+          <div className={styles.contentLeft}>
+            <h2>安全にも配慮</h2>
+            <p>日々の整理整頓だけでなく、地震時にPCが倒れてこないようにするなどの安全対策も5S活動の一環として行います。</p>
           </div>
         </div>
       </div>
-    </>
+    </div>
   )
 }
